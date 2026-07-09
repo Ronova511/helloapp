6539da907f21bac30b3e550285e483ea6f23f786
black <black@example.com>
2026-07-09 18:12:05 +0500

    fixes

--- Program.cs
+++ Program.cs
@@ -6,12 +6,14 @@ namespace HelloAppBlack
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello everyone");
+            GreetWhite();
+            GreetBlack();
         }
 
-        static void SayHello()
+        static void GreetWhite()
         {
             Console.WriteLine("hello white");
-            Console.WriteLine("hello everyone");
         }
 
         static void GreetBlack()
